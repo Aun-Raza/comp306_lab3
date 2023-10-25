decab8be7bcbc24774591b6ad4282815f353d090
Josh H <josh.terance@gmail.com>
2023-10-25 17:39:48 -0400

    Add project files.

---
+++ COMP306_MVC_Lab3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP306_MVC_Lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
