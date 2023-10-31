c5069683dca46185f24d2d3449dff0f39e39f0be
Josh H <josh.terance@gmail.com>
2023-10-31 13:56:48 -0400

    added example to home page to show how to access user data on a page

--- COMP306_MVC_Lab3/Controllers/HomeController.cs
+++ COMP306_MVC_Lab3/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using COMP306_MVC_Lab3.Models;
+﻿using COMP306_MVC_Lab3.Areas.Identity.Data;
+using COMP306_MVC_Lab3.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +9,19 @@ namespace COMP306_MVC_Lab3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        // used to retrieve the logged in user's information
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            this._userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            // This is an example of how to access user ID on a page.
+            ViewData["UserId"] = _userManager.GetUserId(this.User);
             return View();
         }
 
