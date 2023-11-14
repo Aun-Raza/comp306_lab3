9ddff2426906831c43316a35db135860636f426d
Aun-Raza <aunrazaq123@gmail.com>
2023-11-14 16:31:24 -0500

    finished

---
+++ COMP306_MVC_Lab3/ViewModels/MovieVM.cs
@@ -0,0 +1,10 @@
+﻿using COMP306_MVC_Lab3.Models;
+
+namespace COMP306_MVC_Lab3.ViewModels
+{
+    public class MovieVM
+    {
+        public Movie? Movie { get; set; }
+        public List<Review>? Reviews { get; set; }
+    }
+}
